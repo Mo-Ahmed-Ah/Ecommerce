b67f7b87a12bb2eeff69c376ad03100a48c0c261
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2025-07-26 07:20:31 +0300

    add templete

--- DALEcommerce/obj/Debug/net9.0/DALEcommerce.AssemblyInfo.cs
+++ DALEcommerce/obj/Debug/net9.0/DALEcommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DALEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c43fd1be81e502fec6d0741983062fa028726f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DALEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DALEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
